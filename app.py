de1efcab2cbd861ef4d9d0d377dd890fa9e2435b
Olexiy Shumylo <you@example.com>
2024-06-04 15:52:45 +0300

    add post_update

--- app.py
+++ app.py
@@ -38,6 +38,33 @@ def posts_details(id):
 
     return render_template("post_detail.html",article=article)
 
+@app.route('/posts/<int:id>/delete')
+def posts_delete(id):
+    article=Article.query.get_or_404(id)
+    try:
+        db.session.delete(article)
+        db.session.commit()
+        return redirect("/posts")
+    except:
+        return "Error delete arrticle!"
+
+@app.route('/posts/<int:id>/update',methods=['POST','GET'])
+def post_update(id):
+    article = Article.query.get(id)
+    if request.method == "POST":
+        article.title = request.form['title']
+        article.intro = request.form['intro']
+        article.text = request.form['text']
+        try:
+            db.session.commit()
+            return redirect('/posts')
+        except:
+            return "Error update post!"
+    else:
+        return render_template("post_update.html",article=article)
+
+
+
 @app.route('/create-article',methods=['POST','GET'])
 def create_article():
     if request.method == "POST":
